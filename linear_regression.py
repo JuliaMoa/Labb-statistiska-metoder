54f06fac55db8d4eafed4f3161cdfa822e66303c
Julia Sälde <julia.salde@outlook.com>
2026-02-11 12:14:56 +0100

    Complete the required methods. Work on VG demonstration in notebook

--- linear_regression.py
+++ linear_regression.py
@@ -1,18 +1,22 @@
 import numpy as np
+
 import scipy.stats as stats
 
 # på mikaels klass: ändade så att fit tar emot X och y, och att predict tar emot X_new.
 # då måste n och d vara properties som beräknas i fit, så att de kan användas i variance och stddev metoderna. 
+# vrf hade jag property?? 
 
 # class
 class LinearRegression1:
-    def __init__(self): #constructor. self - refers to the object/instance 
+    def __init__(self, alpha=0.05): #constructor. self - refers to the object/instance 
         self.b = None # initialize coefficients to None, will be calculated in fit method
         self._X = None # store training data for later use in variance and stddev calculations
         self._y = None # store target variable for later use in variance and stddev calculations
         self.SSE = None 
         self.Syy = None
         self.XtX_inv = None
+        self.sigma2_hat = None 
+        self.alpha = alpha # significance level for confidence intervals and hypothesis tests, default is 0.05 (5% significance level)
         # Dessa ses av alla metoder! 
 
     @property
@@ -45,13 +49,14 @@ class LinearRegression1:
         # differences between actual and predicted values 
                                         # residuals will be used in variance and stddev calculations
 
-        # SSE and Syy - handy for later
+        # SSE and Syy and sigma2_hat - handy for later
         SSE = np.sum(self.residuals**2) # sum of squared residuals
         self.SSE = SSE
         y_mean = np.mean(self._y)
         Syy = np.sum((self._y - y_mean)**2)
         self.Syy = Syy  # total variation 
-
+        sigma2_hat = self.variance()
+        self.sigma2_hat = sigma2_hat # estimated variance of residuals
         return self 
     
     def predict(self, X_new):
@@ -86,7 +91,8 @@ class LinearRegression1:
         F = (SSR / df_reg) / (self.SSE / df_err)
         # p-value
         p_value = stats.f.sf(F, df_reg, df_err) # survival function for F-distribution
-        return F, p_value 
+        significant = p_value < self.alpha
+        return F, p_value
 
     # method for relevence of the regression 
 
@@ -96,15 +102,24 @@ class LinearRegression1:
 
     # significance test on individual variables - t-test on coefficients
     def t_test_coefficiants(self): 
-        sigma2_hat = self.variance() # estimated variance of residuals
-        t_stats = self.beta.flatten() / np.sqrt(sigma2_hat * np.diag(self.XtX_inv)) # t-statistics for each coefficient
+        t_stats = self.beta.flatten() / np.sqrt(self.sigma2_hat * np.diag(self.XtX_inv)) # t-statistics for each coefficient
         p_values = 2 * stats.t.sf(np.abs(t_stats), df= self.n - self.d - 1) # two-tailed p-values for t-test
-        return t_stats, p_values # t-stats: t-values for each coefficient, p-values: significance of each coefficient
-
-    # method for calculating Pearson number between all pairs of parameters
-
+        significant = p_values < self.alpha 
+        return t_stats, p_values
     
-
+    # method for calculating Pearson r number between all pairs of parameters
+    def Pearson_correlation(self):
+        X_features = self._X[:, 1:] # exclude intercept term
+        corr_matrix = np.corrcoef(X_features, rowvar=False) # correlation matrix for features
+        return corr_matrix
+    
     # confidence intervals on individual parameters
+    def confidence_intervals(self):
+        SE = np.sqrt(np.diag(self.XtX_inv) * self.sigma2_hat) #  vector of standard errors of all coefficients
+        df_residual = self.n - self.d - 1 #degrees of freedom for residuals
+        t_critical = stats.t.ppf(1 - self.alpha/2, df=df_residual) # critical t-value?? for two-tailed test
 
-    # being able to set a confidence interval for the statistics??
+        lower = self.beta.flatten() - t_critical * SE # lower bound of confidence interval for each coefficient
+        upper= self.beta.flatten() + t_critical * SE # upper bound of confidence interval for each coefficient
+        return lower, upper
+    # being able to set a confidence interval for the statistics?? KLART 
